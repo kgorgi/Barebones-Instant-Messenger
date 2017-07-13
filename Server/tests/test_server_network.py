f2d70fc691150edd69b87c19bc351d4d4f54143d
Kian Gorgichuk <kgorgi@users.noreply.github.com>
2017-07-13 13:42:26 -0700

    Temporary Fix to ServerNetworking to allow use developing of unit tests

--- Server/tests/test_server_network.py
+++ Server/tests/test_server_network.py
@@ -5,22 +5,22 @@ import socket
 from networking.server_networking import ServerNetwork
 
 
-class TestRoom(unittest.TestCase):
+class TestServerNetwork(unittest.TestCase):
     def setUp(self):
         self.server_N = ServerNetwork()
         self.client_s = socket.socket()
 
 
     def tearDown(self):
+        del self.server_N
+        self.client_s.close()
+
+    def test_add_client(self):
         address = (socket.gethostname(), 8000)
         try:
             self.client_s.connect(address)
         except ConnectionRefusedError:
             self.fail("Cannot Connect to Server")
-
-
-    def test_add_client(self):
-        pass
 
     def test_send_message(self):
         pass
