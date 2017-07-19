1f54edfa61f46dcdcc3e7ed95d475300432971de
devlyn37 <dev37-2009@hotmail.com>
2017-07-19 16:31:50 -0700

    Leave and Join messages implemented -d

--- Client/clienthandler/client_handler.py
+++ Client/clienthandler/client_handler.py
@@ -77,10 +77,27 @@ class ClientHandler(ClientBackend):
         leave_json = json.dumps(leave)
         self._networkService.send_message(leave_json)
 
+    def quit(self):
+
+        quit = {
+            "command": "Q",
+            "alias": self.alias_name,
+            "address": self.client_address,
+            "room": self.room_name,
+            "message": None
+        }
+
+        quit_json = json.dumps(quit)
+        self._networkService.send_message(quit_json)
+
+
+
     def update(self):
         new_message = self._networkService.receive_next_message()
         if not new_message is None:
             new_message = new_message.rstrip(" ")
+
+
         return new_message
 
     def send_message(self, msg):
